abf092ed2b98eaef2ec12e1c22ecd9619c8d63f9
Aleks Fin <finall.git2022@gmail.com>
2022-06-07 16:04:27 +0300

    Добавлены решения задач

---
+++ Task 1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает 
+// два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 ->  max = 7, min = 5
+//a = -9 b = -3 -> max = -3, min = -9
+
+Console.Write("Введите число а: ");
+string ia = Console.ReadLine();
+int a = int.Parse(ia);
+Console.Write("Введите число b: ");
+string ib = Console.ReadLine();
+int b = int.Parse(ib);
+if(a > b)
+{
+    Console.WriteLine($"Число a = {a} больше числа b = {b}");
+}
+else
+{
+    Console.WriteLine($"Число b = {b} больше числа a = {a}");
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 2: Напишите программу, которая принимает на вход 
+//три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.Write("Введите число а: ");
+string ia = Console.ReadLine();
+int a = int.Parse(ia);
+Console.Write("Введите число b: ");
+string ib = Console.ReadLine();
+int b = int.Parse(ib);
+Console.Write("Введите число c: ");
+string ic = Console.ReadLine();
+int c = int.Parse(ic);
+int max = a;
+if(max < b)
+{
+    max = b;
+}
+if(max < c)
+{
+    max = c;
+}
+Console.Write($"Максимальное число {max}");
