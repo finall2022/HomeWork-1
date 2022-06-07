2d2b2b3992f546f4db93ead203bea63ee96f30e1
Aleks Fin <finall.git2022@gmail.com>
2022-06-07 16:34:01 +0300

    Добавлена дополнительная задача 1

---
+++ AddTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿/* *. Напишите программу вычисления модуля числа.
+    2 -> 2
+    -3 -> 3
+    -7 -> 7 */
+Console.Write("Программа для определения модуля числа");
+Console.Write("Введите число: ");
+string s = Console.ReadLine();
+int n = int.Parse(s);
+int m = 0;
+if (n > 0)
+{
+    Console.Write($"Модуль числа {n} равен {n}");
+}
+else
+{
+    m = n * -1;
+    Console.Write($"Модуль числа {n} равен {m}");
+}
